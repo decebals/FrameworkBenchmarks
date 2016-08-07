9c6c5037fefa859baa0af3aba25c9531df7539a2
Roberto Estivill <robertoestivill@gmail.com>
2016-08-07 19:01:30 +0200

    Adding manual integration test to verify everything works

---
+++ frameworks/Java/pippo-undertow/src/test/java/ro/pippo/benchmark/IntegrationTest.java
@@ -0,0 +1,74 @@
+package ro.pippo.benchmark;
+
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import ro.pippo.benchmark.model.HelloWorld;
+import ro.pippo.core.HttpConstants;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Integration test to verify that everything works.
+ *
+ * Prerequisites:
+ * 1) Have all databases running
+ * 2) Have the server started up
+ */
+public class IntegrationTest {
+
+  public static void main(String[] args) throws IOException {
+    new IntegrationTest().run();
+  }
+
+  private OkHttpClient client;
+
+  private void run() throws IOException {
+    client = new OkHttpClient();
+
+    assertJson(execute("/json"));
+
+    assertJson(execute("/mysql/db"));
+    assertJson(execute("/psql/db"));
+
+    assertJson(execute("/mysql/queries"));
+    assertJson(execute("/psql/queries"));
+
+    assertHtml(execute("/mysql/fortunes"));
+    assertHtml(execute("/psql/fortunes"));
+
+    assertJson(execute("/mysql/updates"));
+    assertJson(execute("/psql/updates"));
+
+    assertHelloWorld(execute("/plaintext"));
+  }
+
+  private void assertHelloWorld(Response response) throws IOException {
+    assertEquals(200, response.code());
+    assertTrue(response.header(HttpConstants.Header.CONTENT_TYPE)
+        .contains(Utils.CONTENT_TYPE_TEXT_PLAIN));
+    assertEquals(HelloWorld.MESSAGE, response.body().string());
+  }
+
+  private void assertJson(Response response) {
+    assertEquals(200, response.code());
+    assertTrue(response.header(HttpConstants.Header.CONTENT_TYPE)
+        .contains(Utils.CONTENT_TYPE_JSON));
+  }
+
+  private void assertHtml(Response response) {
+    assertEquals(200, response.code());
+    assertTrue(response.header(HttpConstants.Header.CONTENT_TYPE)
+        .contains("text/html"));
+  }
+
+  private Response execute(String uri) throws IOException {
+    Request request = new Request.Builder()
+        .url("http://localhost:8080" + uri)
+        .build();
+
+    return client.newCall(request).execute();
+  }
+}
